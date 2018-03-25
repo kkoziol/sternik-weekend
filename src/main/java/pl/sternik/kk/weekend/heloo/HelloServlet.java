fc94f91f0720a194df3a1824bc1d0402f867e6fc
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-25 13:02:18 +0200

    serwlet

---
+++ src/main/java/pl/sternik/kk/weekend/heloo/HelloServlet.java
@@ -0,0 +1,20 @@
+package pl.sternik.kk.weekend.heloo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getOutputStream().println("Hurra moj pierwszy serwlet z obsluga POST --->"
+                + request.getParameter("param1"));
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getOutputStream().println("Hurra moj pierwszy serwlet");
+
+    }
+}
