7137fa752b41b69cb43676095d28f92369620b6e
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-25 13:36:06 +0200

    Filtry

--- src/main/java/pl/sternik/kk/weekend/heloo/HelloServlet.java
+++ src/main/java/pl/sternik/kk/weekend/heloo/HelloServlet.java
@@ -1,5 +1,6 @@
 package pl.sternik.kk.weekend.heloo;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,22 @@ public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("Moj doGET...");
+        
         response.getOutputStream().println("Hurra moj pierwszy serwlet");
 
     }
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        System.out.println("Moj init");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("Moj destroy");
+    }
+    
 }
